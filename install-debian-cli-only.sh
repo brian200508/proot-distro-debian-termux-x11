e3726718087e907eaa82c8b023e877ac907b7a44
brian200508 <36002167+brian200508@users.noreply.github.com>
2026-01-15 15:07:33 +0100

    install Fresh editor

--- install-debian-cli-only.sh
+++ install-debian-cli-only.sh
@@ -248,6 +248,12 @@ proot-distro login debian --user $user_name -- sudo apt update -y
 proot-distro login debian --user $user_name -- sudo apt install -y nodejs npm
 wait_for_key
 
+# Fresh
+banner
+echo "${G}${BOLD} Setting up Node.js..."${W}
+proot-distro login debian --user $user_name -- sudo npm install -g @fresh-editor/fresh-editor
+wait_for_key
+
 # Debian autostart
 banner
 echo "${G}${BOLD} Setting up Debian autostart..."${W}
